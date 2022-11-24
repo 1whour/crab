221ed43ff6499552bdede01c48563ec3e7847884
gnh123 <710390515@qq.com>
2022-11-24 21:41:12 +0800

    up

--- testscript/rm-binary.sh
+++ testscript/rm-binary.sh
@@ -1,10 +1,14 @@
 # https://vocon-it.com/2018/02/15/git-remove-large-binary-files/
-git filter-branch --tag-name-filter 'cat' -f --tree-filter '
-    find . -type d -name binarydir | while read dir
-      do
-        find $dir -type f -name "*.bak" | while read file
-          do
-             git rm -r -f --ignore-unmatch $file
-          done
-      done
-' -- --all
+# git filter-branch --tag-name-filter 'cat' -f --tree-filter '
+#     find . -type d -name binarydir | while read dir
+#       do
+#         find $dir -type f -name "*.bak" | while read file
+#           do
+#              git rm -r -f --ignore-unmatch $file
+#           done
+#       done
+# ' -- --all
+
+#https://www.bilibili.com/read/cv17239503/
+#cluster.log.bak
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *.bak' --prune-empty --tag-name-filter cat -- --all
